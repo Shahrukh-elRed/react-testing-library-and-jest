86505beb3d147462b6ba0780a9536b10dab1a280
Shahrukh <shahrukh1494@gmail.com>
2024-06-13 15:27:12 +0530

    first react test case

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>First React Test Case</p>
+      <p>Shahrukh Khan</p>
+      <img
+        title="AI generated image"
+        src="https://images.tech.co/wp-content/uploads/2023/11/21202640/AI-generated-landscape.jpg"
+        alt="AI"
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,10 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/First React Test Case/i); // ignores matching exact text case
+  const text2 = screen.getByText("Shahrukh Khan"); // matches text case
+  const title = screen.getByTitle("AI generated image");
+  expect(text).toBeInTheDocument();
+  expect(text2).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
 });
