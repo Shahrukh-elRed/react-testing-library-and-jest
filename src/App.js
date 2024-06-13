97a4c4284100c775485436fd5d4235f87f7f9b43
Shahrukh <shahrukh1494@gmail.com>
2024-06-13 15:49:36 +0530

    testing input field

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
     <div className="App">
       <p>First React Test Case</p>
       <p>Shahrukh Khan</p>
+      <input
+        type="text"
+        placeholder="Enter User Name"
+        name="username"
+        id="userId"
+        value="shahrukh"
+      />
+      <br />
+      <br />
+      <br />
+
       <img
         title="AI generated image"
         src="https://images.tech.co/wp-content/uploads/2023/11/21202640/AI-generated-landscape.jpg"
--- src/App.test.js
+++ src/App.test.js
@@ -10,3 +10,15 @@ test("renders learn react link", () => {
   expect(text2).toBeInTheDocument();
   expect(title).toBeInTheDocument();
 });
+
+test("Testing Input box", () => {
+  render(<App />);
+  const checkInput = screen.getByRole("textbox");
+  const checkInputPlaceholder = screen.getByPlaceholderText("Enter User Name");
+  expect(checkInput).toBeInTheDocument();
+  expect(checkInputPlaceholder).toBeInTheDocument();
+  expect(checkInput).toHaveAttribute("name", "username");
+  expect(checkInput).toHaveAttribute("id", "userId");
+  expect(checkInput).toHaveAttribute("type", "text");
+  expect(checkInput).toHaveAttribute("value", "shahrukh");
+});
