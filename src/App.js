48a0455af5ac6ec9e2d9dbcc63f660c01a2729fa
Shahrukh <shahrukh1494@gmail.com>
2024-06-14 18:46:52 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>RTL Query: getByLabelText</h1>
-      <label htmlFor="user-name">Username</label>
-      <input type="text" id="user-name" defaultValue="shahrukh" />
-      <label htmlFor="skills">Skills</label>
-      <input type="checkbox" id="skills" />
+      <h1>RTL Query: getAllByLabelText</h1>
     </div>
   );
 }
