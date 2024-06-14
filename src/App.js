7b6360dfca388d491dd7dfa162be7dcf1f247460
Shahrukh <shahrukh1494@gmail.com>
2024-06-14 18:04:27 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Multiple Item with Role</h1>
-      <h1>Custom Role</h1>
-      <button>Click me 1</button>
-      <button>Click me 2</button>
-      <label htmlFor="input1">User Name</label>
-      <input type="text" id="input1" />
-      <label htmlFor="input2">User Age</label>
-      <input type="text" id="input2" />
-      <div role="dummy">Dummy Text</div>
+      <h1>RTL Query : getAllByRole</h1>
     </div>
   );
 }
