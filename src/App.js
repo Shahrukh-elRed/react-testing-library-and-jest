2701591599992d3a79ba63c701cd7e9d4d984845
Shahrukh <shahrukh1494@gmail.com>
2024-06-14 16:01:03 +0530

    getByRole

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>getByRole</h1>
+      <input type="text" defaultValue="hello" disabled />
+      <button>Click me!</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("get by role", () => {
+  render(<App />);
+  const inputField = screen.getByRole("textbox");
+  const btn = screen.getByRole("button");
+  expect(inputField).toBeInTheDocument();
+  expect(inputField).toHaveValue("hello");
+  expect(inputField).toBeDisabled();
+  expect(btn).toBeInTheDocument();
+});
