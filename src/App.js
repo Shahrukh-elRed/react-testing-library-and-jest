398cd5e1bf36a0294c7ddc56c573f9f59d6fd311
Shahrukh <shahrukh1494@gmail.com>
2024-06-14 18:41:06 +0530

    getByLabelText

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <h1>RTL Query: getByLabelText</h1>
+      <label htmlFor="user-name">Username</label>
+      <input type="text" id="user-name" defaultValue="shahrukh" />
+      <label htmlFor="skills">Skills</label>
+      <input type="checkbox" id="skills" />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
+test("getByLabelText test case 1", () => {
+  render(<App />);
+  const input = screen.getByLabelText("Username");
+  expect(input).toBeInTheDocument();
+  expect(input).toHaveValue("shahrukh");
+});
+
+test("getByLabelText test case 2", () => {
+  render(<App />);
+  const checkbox = screen.getByLabelText("Skills");
+  expect(checkbox).toBeInTheDocument();
+});
