6aac8437548496b1f6c7821bcbba1c2e88bae340
Shahrukh <shahrukh1494@gmail.com>
2024-06-14 16:06:01 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>getByRole</h1>
-      <input type="text" defaultValue="hello" disabled />
-      <button>Click me!</button>
+      <h1>Multiple Item with Role</h1>
+      <h1>Custom Role</h1>
     </div>
   );
 }
