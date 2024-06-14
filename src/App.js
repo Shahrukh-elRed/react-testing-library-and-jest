00d5ea51f2a7c86a55ae95ad254ec5bcf7aa4b65
Shahrukh <shahrukh1494@gmail.com>
2024-06-14 18:18:55 +0530

    getAllByRole

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <div className="App">
       <h1>RTL Query : getAllByRole</h1>
+      <button>Click Me</button>
+      <button>Click Me</button>
+      <button>Click Me</button>
+      <button>Click Me</button>
+      <button>Click Me</button>
+      <button>Click Me</button>
+      <select>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+      </select>
     </div>
   );
 }
