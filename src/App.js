0a4f22c7f4942506fb377ce93a9902b8c90fb4d9
Shahrukh <shahrukh1494@gmail.com>
2024-06-15 10:27:52 +0530

    getByText and getAllByText

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>RTL Query: getByText and getAllByText</h1>
+      <button>Login</button>
+      <p className="para-style" id="p1">
+        P tag testing
+      </p>
+      <h1>Heading tag</h1>
+      <h1>Heading tag</h1>
     </div>
   );
 }
