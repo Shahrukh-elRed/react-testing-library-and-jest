1637d92c3aa5ef01824d13ad3312c66b092a815c
Shahrukh <shahrukh1494@gmail.com>
2024-06-15 12:56:09 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>RTL Query: getByTestId and getAllByTestId</h1>
-      <div data-testid="div-test-id">Testing with test id</div>
-      <div data-testid="div-test-id">Testing with test id</div>
-
-      <div data-testid="h2-test-id">h2 tag testing</div>
+      <h1>Overriding data-testid</h1>
+      <div data-testid="test-div">Dummy text</div>
     </div>
   );
 }
