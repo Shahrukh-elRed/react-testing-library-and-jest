37604d4408a1d18be643695aed6af97dc1a39cb9
Shahrukh <shahrukh1494@gmail.com>
2024-06-15 14:01:50 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>getByTitle and getAllByTitle</h1>
-      <button title="click me">Click me</button>
-      <br />
-      <span title="black spades suit">&#x2660;</span>
-      <span title="black spades suit">&#x2660;</span>
+      <h1>getByAltText and getAllByAltText</h1>
     </div>
   );
 }
