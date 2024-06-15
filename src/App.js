bdd04e56368e593dd041d2a7a1c439155165198d
Shahrukh <shahrukh1494@gmail.com>
2024-06-15 12:31:10 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>RTL Query: getByText and getAllByText</h1>
-      <button>Login</button>
-      <p className="para-style" id="p1">
-        P tag testing
-      </p>
-      <h1>Heading tag</h1>
-      <h1>Heading tag</h1>
+      <h1>RTL Query: getByTestId and getAllByTestId</h1>
     </div>
   );
 }
