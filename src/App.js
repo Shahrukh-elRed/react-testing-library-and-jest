ba88905b9c3d4fa0f842e2993b717b01533bf5b5
Shahrukh <shahrukh1494@gmail.com>
2024-06-15 13:42:08 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>getByDisplayValue and getAllByDisplayValue</h1>
-      <input type="text" defaultValue="shahrukh" />
-      <textarea defaultValue="shahrukh" />
-
-      <textarea defaultValue="shahrukh khan" />
-      <input type="radio" defaultValue="male" />
+      <h1>getByTitle and getAllByTitle</h1>
     </div>
   );
 }
