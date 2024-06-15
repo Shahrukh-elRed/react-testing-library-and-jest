9218c6d97e671589c6d2a7fdff777d8881a4ca44
Shahrukh <shahrukh1494@gmail.com>
2024-06-15 14:13:34 +0530

    getByAltText and getAllByAltText

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="App">
       <h1>getByAltText and getAllByAltText</h1>
+      <img
+        src="https://kupidonia.com/content/quiz/photo/big/2306_1.jpg"
+        alt="test img"
+      />
+      <img
+        src="https://kupidonia.com/content/quiz/photo/big/2306_1.jpg"
+        alt="test img"
+      />
     </div>
   );
 }
