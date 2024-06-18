fb2d464b3859b7926f6462c4635eaa535d253a1c
Shahrukh <shahrukh1494@gmail.com>
2024-06-18 16:38:46 +0530

    onChange event testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+
   return (
     <div className="App">
       <h1>onChange Event Testing</h1>
+      <h2>{name}</h2>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        placeholder="enter name"
+      />
     </div>
   );
 }
