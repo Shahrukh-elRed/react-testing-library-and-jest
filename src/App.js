634478edc5ef009186ff58823e2ec59021b46cfc
Shahrukh <shahrukh1494@gmail.com>
2024-06-18 11:04:34 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Assertion Methods</h1>
-      <input
-        type="text"
-        defaultValue="shahrukh khan"
-        name="userName"
-        className="test-style"
-        id="user-name"
-      />
-      <button className="click-me-button">Click Me</button>
+      <h1>TextMatch with String and Regex</h1>
     </div>
   );
 }
