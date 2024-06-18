d70213b3354ef81350271009b1570ffde2bc5762
Shahrukh <shahrukh1494@gmail.com>
2024-06-18 09:48:22 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>getByAltText and getAllByAltText</h1>
-      <img
-        src="https://kupidonia.com/content/quiz/photo/big/2306_1.jpg"
-        alt="test img"
-      />
-      <img
-        src="https://kupidonia.com/content/quiz/photo/big/2306_1.jpg"
-        alt="test img"
-      />
+      <h1>Assertion Methods</h1>
     </div>
   );
 }
