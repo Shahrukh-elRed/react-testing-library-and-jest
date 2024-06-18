b068bdafd6edcb7b9d486b8c5a7e27718d66db81
Shahrukh <shahrukh1494@gmail.com>
2024-06-18 12:15:21 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>TextMatch with Function</h1>
-      <div>Hello World</div>
+      <h1>queryBy and queryAllBy</h1>
     </div>
   );
 }
