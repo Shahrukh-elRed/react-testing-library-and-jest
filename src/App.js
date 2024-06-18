cba750c001fae063778ff654defbb0f5697c191e
Shahrukh <shahrukh1494@gmail.com>
2024-06-18 13:26:24 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setData(true);
-    }, 1000);
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>findBy and findAllBy</h1>
-      {data ? <h2>data found</h2> : <h2>No data found</h2>}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("testing element with findBy", async () => {
-  render(<App />);
-  const el = await screen.findByText("data found", {}, { timeout: "2000" });
-  expect(el).toBeInTheDocument();
-});
