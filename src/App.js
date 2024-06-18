577dd6265f51a157e7a59d04b8334248d388c1dd
Shahrukh <shahrukh1494@gmail.com>
2024-06-18 17:26:21 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-
   return (
     <div className="App">
-      <h1>onChange Event Testing</h1>
-      <h2>{name}</h2>
-      <input
-        type="text"
-        onChange={(e) => setName(e.target.value)}
-        placeholder="enter name"
-      />
+      <h1>Props Testing</h1>
     </div>
   );
 }
