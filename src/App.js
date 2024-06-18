5d21fdcad21fd9d50c4169770b25685f42693607
Shahrukh <shahrukh1494@gmail.com>
2024-06-18 15:24:23 +0530

    querying within elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        Hello World
+        <p>hi</p>
+        <p>hello</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
