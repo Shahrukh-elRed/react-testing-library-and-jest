9ff53beb7cbf7b0bc861d96edca5d15217df2419
Shahrukh <shahrukh1494@gmail.com>
2024-06-18 10:30:46 +0530

    assertion methods and negative assertions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="App">
       <h1>Assertion Methods</h1>
+      <input
+        type="text"
+        defaultValue="shahrukh khan"
+        name="userName"
+        className="test-style"
+        id="user-name"
+      />
+      <button className="click-me-button">Click Me</button>
     </div>
   );
 }
