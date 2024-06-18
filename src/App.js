de7d8e9557e72d2c10e2e71fca496bab143ecf92
Shahrukh <shahrukh1494@gmail.com>
2024-06-18 15:54:41 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        Hello World
-        <p>hi</p>
-        <p>hello</p>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
