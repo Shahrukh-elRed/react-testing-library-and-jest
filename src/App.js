c7e15e3e78cbc95149c0490ddc44d4718c6f7792
Shahrukh <shahrukh1494@gmail.com>
2024-06-18 18:33:49 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
 
 function App({ testFunction }) {
-  return (
-    <div className="App">
-      <h1>Functional Props Testing and Mocking</h1>
-      <button onClick={testFunction}>Click</button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
