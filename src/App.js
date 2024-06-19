aaf43cc457947075b574eb08f6133cc1c69c4239
Shahrukh <shahrukh1494@gmail.com>
2024-06-19 10:15:26 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <button onClick={() => setCount((count) => count + 1)}>
-        Click to increase : {count}
-      </button>
-      <h2>Heading 2</h2>
-      <h5>Heading 5</h5>
-      <input type="text" />
+      <h1>API call</h1>
     </div>
   );
 }
