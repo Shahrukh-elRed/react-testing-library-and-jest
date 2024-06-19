08eaa08461deb2c97dc4862ec7bb83e94b6911b0
Shahrukh <shahrukh1494@gmail.com>
2024-06-19 12:58:18 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = async () => {
-    let result = await fetch("https://jsonplaceholder.typicode.com/users");
-    result = await result.json();
-    setData(result);
-  };
-
-  return (
-    <div>
-      <h1>List of Users</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
